c77b95f1b4f5ffe1db351c2d6184a338a2b996d5
alokp000 <118658978+alokp000@users.noreply.github.com>
2024-08-11 18:08:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -6,13 +6,13 @@ terraform {
       
     }
   }
-  backend "s3" {
-    bucket         = "mern-1bucket"
-    region         = "ap-south-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "merntable"
-    encrypt        = true
-  }
+  # backend "s3" {
+   # bucket         = "mern-1bucket"
+   # region         = "ap-south-1"
+   # key            = "eks/terraform.tfstate"
+   # dynamodb_table = "merntable"
+   # encrypt        = true
+ # }
 }
 
 provider "aws" {
