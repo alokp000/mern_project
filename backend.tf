b3f02b85a32c95145acc26ca8ea932dec26debd4
alok <pandaalok000@gmail.com>
2024-08-11 17:19:30 +0530

    stagged

---
+++ backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "mern-1bucket"
+    region         = "ap-south-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "merntable"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region  = var.aws-region
+}
