3bed92d46bbe46ad6fea74aa502fdeea1cfe712a
alokp000 <118658978+alokp000@users.noreply.github.com>
2024-08-11 18:11:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,20 +1,20 @@
-terraform {
+# terraform {
   
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
+ # required_providers {
+  #  aws = {
+   #   source  = "hashicorp/aws"
       
-    }
-  }
-  # backend "s3" {
+ #   }
+ # }
+ #  backend "s3" {
    # bucket         = "mern-1bucket"
    # region         = "ap-south-1"
-   # key            = "eks/terraform.tfstate"
-   # dynamodb_table = "merntable"
-   # encrypt        = true
+  #  key            = "eks/terraform.tfstate"
+  #  dynamodb_table = "merntable"
+  #  encrypt        = true
  # }
-}
+#}
 
-provider "aws" {
-  region  = var.aws-region
-}
+#provider "aws" {
+ # region  = var.aws-region
+#}
